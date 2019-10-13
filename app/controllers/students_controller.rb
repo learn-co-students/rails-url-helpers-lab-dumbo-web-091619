d2d2fcb3ff432216a3d6c22b240ab475a7c155dc
awazienommy <coawazie@gmail.com>
2019-10-12 20:08:37 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,16 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    #we cal the set_student private method to load the student with given id
+    set_student
+    #now updatet he 'active' attribute of that student
+    @student.update(active: !@student.active)
+    #finally send to that students show rendering to display student with updated attributes
+    redirect_to student_path(@student)
   end
 
   private
